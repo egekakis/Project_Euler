4695da214afc8492e3d7fab9270f29dd97a4893d
Eamon Gekakis <89441256+egekakis@users.noreply.github.com>
2023-08-23 01:32:02 -0400

    Add files via upload

--- PE8/Project_Euler_8.m
+++ PE8/Project_Euler_8.m
@@ -1,7 +1,16 @@
 clear,clc
-load num.mat
-skele = ones((length(num)-13),1);
-for i = 1:(length(num)-13)
-    skele(i,:) = skele(i,:).*str2double(num(i:(i+12)));
+tic
+load num.mat %loaded in the 1000 digit # from .mat file 
+skele = ones((length(num)-13),1); %creates an array of ones equal to the amount of 13 digit sequences possible in a 1000 digit number
+for i = 1:(length(num)-13) %iterating over the length of possible 13 digit sequences 
+    skele(i,:) = skele(i,:).*str2double(num(i:(i+12))); %converts the imported number to a double from string, sorts into 13 digit sequences and appends to ones array
 end
-num2str(skele);
+ID = skele > 1e12; %removes all numbers that start with '0' (needed because of string import)
+skele = skele(ID);
+skele = num2str(skele); %converts char array back to string 
+skin = ones(length(skele),1);
+for ii = 1:(length(skele))
+    skin(ii,:) = prod(skele(ii,:)-'0'); %uses product function to multiply all digits in the 13 digit sequence
+end
+disp(max(skin)) %finds largest value in array and displays 
+toc
